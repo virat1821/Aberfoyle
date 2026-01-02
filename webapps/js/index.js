c05aea33a62768f47f1e75e586ca2327d5669f9f
virat1821 <vishnuprakash9597@gmail.com>
2026-01-02 16:35:13 +0530

    Initial website upload

---
+++ webapps/js/index.js
@@ -0,0 +1,10 @@
+
+// ABOUT ABERFOYLE INN 
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".about-card, .about-image-wrap")
+      .forEach(el => el.style.opacity = 1);
+  });
+
+
+
+
