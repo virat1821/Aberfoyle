236d9b112760b21a81aef591e444b6b9545a8e38
virat1821 <vishnuprakash9597@gmail.com>
2026-02-17 21:07:07 +0530

    17

--- webapps/js/index.js
+++ webapps/js/index.js
@@ -7,6 +7,21 @@
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navbar-collapse .nav-link");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  navLinks.forEach(function(link) {
+    link.addEventListener("click", function () {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      bsCollapse.hide();
+    });
+  });
+});
+
+
 
 
 
